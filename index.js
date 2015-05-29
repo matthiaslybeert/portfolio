395f29ec09f156d479613ccdcf4fa4a1304b4a45
Matthias Lybeert <matthias.l@hotmail.com>
2015-05-29 15:38:59 +0000

    mijn eerste commit

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+    	navigation: true,
+    	anchors:['intro', 'about', 'gallery', 'contact'],
+    	verticalCentered: true
+    });
+});
+
